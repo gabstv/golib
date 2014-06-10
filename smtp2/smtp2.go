6804fdccdbc20b935c7ad78fb36967ca4da51a5b
Gabriel Ochsenhofer <gabs@gabs.tv>
2014-06-10 20:18:50 -0300

    stream smtp messages instead of sending a byte slice

---
+++ smtp2/smtp2.go
@@ -0,0 +1,50 @@
+package smtp2
+
+import (
+	"io"
+	"net/smtp"
+)
+
+func SendMail(addr string, a smtp.Auth, from string, to []string, msg io.Reader) error {
+	c, err := smtp.Dial(addr)
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+	if err = c.Hello("localhost"); err != nil {
+		return err
+	}
+	if ok, _ := c.Extension("STARTTLS"); ok {
+		if err = c.StartTLS(nil); err != nil {
+			return err
+		}
+	}
+	if a != nil {
+		if ok, _ := c.Extension("AUTH"); ok {
+			if err = c.Auth(a); err != nil {
+				return err
+			}
+		}
+	}
+	if err = c.Mail(from); err != nil {
+		return err
+	}
+	for _, addr := range to {
+		if err = c.Rcpt(addr); err != nil {
+			return err
+		}
+	}
+	w, err := c.Data()
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(w, msg)
+	if err != nil {
+		return err
+	}
+	err = w.Close()
+	if err != nil {
+		return err
+	}
+	return c.Quit()
+}
