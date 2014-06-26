45f3384eb36575aa943792fd6ad1b5a4cc8914b4
Gabriel Ochsenhofer <gabs@gabs.tv>
2014-06-26 16:16:57 -0300

    mais testes

--- mailmessage/reader_test.go
+++ mailmessage/reader_test.go
@@ -6,9 +6,15 @@ import (
 )
 
 func testQP(t *testing.T) {
-	hdr := mail.Header{"From": []string{"=?UTF-8?Q?ANT=C3=94NIA_FERNANDES_PESSOA_SENA?= <senna@gmail.com>"}}
+	hdr := mail.Header{
+		"From":  []string{"=?UTF-8?Q?ANT=C3=94NIA_FERNANDES_PESSOA_SENA?= <senna@gmail.com>"},
+		"From2": []string{"=?UTF-8?Q?image=2Ejpg?=_0.jpg =?UTF-8?Q?image=2Ejpg?=_0.jpg"},
+	}
 	normalizeHeaders(hdr)
 	if hdr.Get("From") != "ANTÔNIA_FERNANDES_PESSOA_SENA <senna@gmail.com>" {
 		t.Errorf("%v != %v ", hdr.Get("From"), "ANTÔNIA_FERNANDES_PESSOA_SENA <senna@gmail.com>")
+	}
+	if hdr.Get("From2") != "image.jpg_0.jpg image.jpg_0.jpg" {
+		t.Errorf("%v != %v ", hdr.Get("From2"), "image.jpg_0.jpg image.jpg_0.jpg")
 	}
 }
