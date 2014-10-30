2d1174e26163bb32e82859d9d75c01999deac9a9
Gabriel Ochsenhofer <gabs@gabs.tv>
2014-10-30 15:49:20 -0200

    gstr

---
+++ gstr/gstr.go
@@ -0,0 +1,36 @@
+package gstr
+
+import (
+	"strings"
+)
+
+func Split3(s, sep string) (a, b, c string) {
+	s0 := strings.Split(s, sep)
+	if len(s0) >= 3 {
+		c = s0[2]
+		b = s0[1]
+		a = s0[0]
+		return
+	} else if len(s0) >= 2 {
+		b = s0[1]
+		a = s0[0]
+		return
+	} else if len(s0) >= 1 {
+		a = s0[0]
+		return
+	}
+	return
+}
+
+func Split2(s, sep string) (a, b string) {
+	s0 := strings.Split(s, sep)
+	if len(s0) >= 2 {
+		b = s0[1]
+		a = s0[0]
+		return
+	} else if len(s0) >= 1 {
+		a = s0[0]
+		return
+	}
+	return
+}
